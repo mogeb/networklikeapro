fd2f61b6dd2f969d3809785a90e34bc570842af1
Mohamad Gebai <mogeb@fb.com>
2020-05-30 12:05:34 -0400

    Added dropdown

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,5 @@
-var quotes = ['hello all my name is {name} and i work at {company}', 'hi everyeone my name be {name} me works at {company}',
-    'whatup my man me name {name} i hustle at {company}', 'ayo dude i\'m {name} i work at {company}'];
+// var quotes = ['hello all my name is {name} and i work at {company}', 'hi everyeone my name be {name} me works at {company}',
+//     'whatup my man me name {name} i hustle at {company}', 'ayo dude i\'m {name} i work at {company}'];
 
 var jobStartQuotes = ['im {name} im starting at {company}', 'me name {name} me works at {company}'];
 var jobQuitQuotes = ['im {name} im leaving at {company}', 'me name {name} me leaving {company}'];
@@ -29,10 +29,30 @@ conferenceInput.addEventListener('input', evt => {
 });
 
 function newQuote() {
+    var quotes;
+    var category = document.getElementById("category").value;
+
+    if (category == 'jobStart') {
+        quotes = jobStartQuotes;
+    } else if (category == 'jobQuit') {
+        quotes = jobQuitQuotes;
+    } else if (category == 'anniversary') {
+        quotes = celebrateAnniversaryQuotes;
+    } else if (category == 'conference') {
+        quotes = conferenceSpeakingQuotes;
+    }
+    
     var rand = Math.floor(Math.random() * quotes.length);
     var quote = quotes[rand];
     quote = quote.replace('{name}', name);
     quote = quote.replace('{company}', company);
+    quote = quote.replace('{conference}', conference);
+
     document.getElementById('quoteDisplay').innerHTML = quote;
+    console.log(category);
     console.log('neq qyote');
+}
+
+function dropDownFunction() {
+    console.log('dropdown');
 }
