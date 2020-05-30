a0b82c1106c1f5e54bea879aab9df0475404b9f5
Mohamad Gebai <mogeb@fb.com>
2020-05-30 11:53:27 -0400

    Prepare for categories

--- js/quotes.js
+++ js/quotes.js
@@ -1,8 +1,32 @@
 var quotes = ['hello all my name is {name} and i work at {company}', 'hi everyeone my name be {name} me works at {company}',
     'whatup my man me name {name} i hustle at {company}', 'ayo dude i\'m {name} i work at {company}'];
 
+var jobStartQuotes = ['im {name} im starting at {company}', 'me name {name} me works at {company}'];
+var jobQuitQuotes = ['im {name} im leaving at {company}', 'me name {name} me leaving {company}'];
+var celebrateAnniversaryQuotes = ['im {name} im celebrating at {company}', 'me name {name} me celebrates at {company}'];
+var conferenceSpeakingQuotes = ['im {name} im speaking at {conference}', 'me name {name} me speaks at {conference}'];
+
 var name = '';
 var company = '';
+var conference = '';
+
+var nameInput = document.getElementById('nameInput');
+nameInput.addEventListener('input', evt => {
+    console.log('name input = ' + nameInput.value);
+    name = nameInput.value;
+});
+
+var companyInput = document.getElementById('companyInput');
+companyInput.addEventListener('input', evt => {
+    console.log('company input = ' + companyInput.value);
+    company = companyInput.value;
+});
+
+var conferenceInput = document.getElementById('conferenceInput');
+conferenceInput.addEventListener('input', evt => {
+    console.log('conference input = ' + conferenceInput.value);
+    company = conferenceInput.value;
+});
 
 function newQuote() {
     var rand = Math.floor(Math.random() * quotes.length);
@@ -12,18 +36,3 @@ function newQuote() {
     document.getElementById('quoteDisplay').innerHTML = quote;
     console.log('neq qyote');
 }
-
-var nameInput = document.getElementById('nameInput');
-
-nameInput.addEventListener('input', evt => {
-    console.log('name input = ' + nameInput.value);
-    name = nameInput.value;
-});
-
-
-var companyInput = document.getElementById('companyInput');
-
-companyInput.addEventListener('input', evt => {
-    console.log('company input = ' + companyInput.value);
-    company = companyInput.value;
-});
